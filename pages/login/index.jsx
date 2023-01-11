0a1796fe6cb004b66510e6e571ed265274b113d5
TienDat1999 <letiendat1999py@gmail.com>
2023-01-11 17:09:38 +0700

    second commit

---
+++ pages/login/index.jsx
@@ -0,0 +1,50 @@
+
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+
+export default function Login() {
+  return (
+    <div>
+      <h1>HI I'm from login page</h1>
+      <div className="App">
+      <Grid>
+        <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
+          <CardContent>
+
+            <form>
+              <Grid container spacing={1}>
+                <Grid xs={12} item>
+                  <TextField
+                    placeholder="user name"
+                    label="User Name"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    placeholder="Enter Password"
+                    label="Password"
+                    variant="outlined"
+                    fullWidth
+                    required
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                <Button variant="outlined">Submit</Button>
+                </Grid>
+              </Grid>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+    </div>
+    </div>
+  );
+}
