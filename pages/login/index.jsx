db4021b9d811094057b1c5ca2ca7aff522ee2294
ltdat7 <ltdat7@cmcglobal.vn>
2023-01-31 11:06:28 +0700

    adding login behavior

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -3,11 +3,26 @@ import TextField from '@mui/material/TextField';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-
+import { useState } from 'react';
 
 export default function Login() {
+  const [userName, setUserName] = useState('')
+  const [password, setPassword] = useState('')
+  const onLogin = (event) => {
+    event.preventDefault()
+    console.log('userName', userName);
+    console.log('password', password);
+  }
+  
+  const onUserNameChange = (event) => {
+    setUserName(event.target.value);
+  }
+
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
   return (
     <div>
       <h1>LOGIN</h1>
@@ -15,7 +30,6 @@ export default function Login() {
       <Grid>
         <Card style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
           <CardContent>
-
             <form>
               <Grid container spacing={1}>
                 <Grid xs={12} item>
@@ -25,6 +39,8 @@ export default function Login() {
                     variant="outlined"
                     fullWidth
                     required
+                    value={userName}
+                    onChange={onUserNameChange}
                   />
                 </Grid>
                 <Grid item xs={12}>
@@ -34,10 +50,12 @@ export default function Login() {
                     variant="outlined"
                     fullWidth
                     required
+                    value={password}
+                    onChange={onPasswordChange}
                   />
                 </Grid>
                 <Grid item xs={12}>
-                <Button variant="outlined">Submit</Button>
+                <Button type='submit' variant="outlined" onClick={onLogin}>Submit</Button>
                 </Grid>
               </Grid>
             </form>
